212899507b40a5906334f226340b9cad8256e7f1
James Maidment <jmaidment@gmail.com>
2014-06-01 10:13:32 +1000

    Fixed login

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,51 +4,49 @@ $(document).ready(function() {
     FB.init({
       appId: '1427999834129994',
     });     
-    $('#loginbutton,#feedbutton').removeAttr('disabled');
     FB.getLoginStatus(updateStatusCallback);
   });
 
   function updateStatusCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
-      loggedIn();
+      console.log('Welcome!  Fetching your information.... ');
+      FB.api('/me', function(response) {
+        addUser(response);
+        console.log('Successful login for: ' + response.name);
+        $('#status').html('Thanks for signing up, ' + response.first_name + '!');
+        $('#login').html('Welcome to the Tribe! <span class="glyphicon glyphicon-ok">').addClass('btn-success');
+      });
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
+      $('#status').html('');
+      $('#login').html('Sign up with Facebook <span class="glyphicon glyphicon-user">').removeClass('btn-success').addClass('btn-primary');
     }
   }
 
   $("#login").click(function() {
      FB.login(function(response) {
-      Log.info('FB.login callback', response);
       if (response.status === 'connected') {
-        Log.info('User is logged in');
         addUser(response);
+        updateStatusCallback(response);
       } else {
         Log.info('User is logged out');
       }
     });
    });
 
-  // Here we run a very simple test of the Graph API after login is
-  // successful.  See statusChangeCallback() for when this call is made.
-  function loggedIn() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-              addUser(response);
-      console.log('Successful login for: ' + response.name);
-      $('#status').html('Thanks for signing up, ' + response.first_name + '!');
-      $('#login').html('Welcome to the Tribe! <span class="glyphicon glyphicon-ok">').addClass('btn-success');
+  $("#logout").click(function() {
+    FB.logout(function(response) {
+      updateStatusCallback(response);
     });
-  }
+  });
 
   function addUser(response) {
     console.log('Adding user to database.... ');
